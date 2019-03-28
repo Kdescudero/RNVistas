d6169b6341422210dd04172a2e95a532cc637f39
kevin <kdescudero01@gmail.com>
2019-03-28 09:06:40 -0500

    Header Ready

--- src/tableWork-7/dumps/header.js
+++ src/tableWork-7/dumps/header.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const Header=()=>(
     <View style={styles.container}>
         <Icon name={'menu'} size={32}/>
-        <Text>Logo</Text>
+        <Image source={require('../../../assets/logos/Logo-triko-facebook.png')} style={styles.logo}/>
         <Icon name={'person-outline'} size={32}/>
     </View>
 )
 
 const styles = StyleSheet.create({
     container : {
-        borderWidth : 1,
-        borderColor : 'red',
-
         width           : '100%',
         display         : 'flex',
         alignItems      : 'center',
@@ -22,7 +19,11 @@ const styles = StyleSheet.create({
         flexDirection   : 'row',
         padding         : 12,
         // backgroundColor : '#ececec'
-    }    
+    },
+    logo : {
+        width  : '35%',
+        height : 50,
+    }
 })
 
 export default Header;
